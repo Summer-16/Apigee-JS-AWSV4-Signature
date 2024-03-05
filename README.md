7bf5c0a98117b2095d9f1ebdd28c7de195214b84
Shivam Parashar <shivam.parashar@monotype.com>
2024-03-05 11:00:33 +0530

    Initial Commit, Libs added

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Apigee-JS-AWSV4-Signature
+
+awsSignV4Apigee.js is a Pure JavaScript implementation of AWS Signature Version 4 signing process for use with Amazon Web Services (AWS) APIs. 
+
+This is a custom implementation which uses Googles's SHA256 libraries to specifically work with Apigee Edge/Apigee X or inside a vanilla JavaScript Environment.
+
+## Usage
+Just load all the files in the `src` folder into your project and use the `awsSignV4Apigee` function to sign your requests.
+
+### Simple Example
+```
+const signData = {
+  request: {
+    method: 'POST',
+    params: "",
+    body: encodedMessageBody
+  },
+  config: {
+    host: sqsHost,
+    uri: sqsURI,
+    accessKey: sqsAccessKey,
+    secretKey: sqsSecretKey,
+    region: sqsRegion,
+    service: "sqs"
+  }
+};
+
+var signedHeaders = awsSignV4Apigee.createSignature(signData);
+console.log(signedHeaders);
+/*
+{
+  authorization : "AWS4-HMAC-SHA256 Credential=AKIA6DAAAAWLWABC3ADC/20240305/us-east-1/sqs/aws4_request, SignedHeaders=host;x-amz-date, Signature=109301af24018edfd27e8bb182asdfdsdfdss9b98e8ac9af63c62399295fdc3d"
+  host : "sqs.us-east-1.amazonaws.com"
+  xAmzDate : "20240305T051902Z"
+}
+*/
+
+```
+
+### For Apigee proxy example check the `example` folder.
+
+Note: The implementation uses the `var` keyword for defining variables. If you are using this in a modern environment, you can replace `var` with `let` or `const` as per your requirement. I have used `var` for compatibility with Apigee environment.
---
+++ src/awsSignV4Apigee.js
@@ -0,0 +1,103 @@
+; (function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    /* global define */
+    define(['crypto-js/core', 'crypto-js/sha256', 'crypto-js/hmac-sha256'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('crypto-js/core'),
+      require('crypto-js/sha256'),
+      require('crypto-js/hmac-sha256'));
+  } else {
+    /* global CryptoJS */
+    root.awsSignV4Apigee = factory(CryptoJS, CryptoJS.SHA256, CryptoJS.HmacSHA256);
+  }
+}(this, function (CryptoJS) {
+
+  function createSignature(params) {
+
+    // Extract the parameters
+    var { request, config } = params;
+
+    // Extract the configuration
+    var {
+      host,
+      uri,
+      accessKey,
+      secretKey,
+      region = "us-east-1",
+      service
+    } = config;
+
+    // Algorithm used for signing
+    var algorithm = 'AWS4-HMAC-SHA256';
+
+    // Get the current timestamp in UTC
+    var now = new Date();
+    var amzDate = now.toISOString().replace(/[:-]|\.\d{3}/g, '');
+
+    // Create a date string for the credential scope
+    var dateStamp = amzDate.substr(0, 8);
+    var credentialScope = dateStamp + '/' + region + '/' + service + '/aws4_request';
+
+    // Canonical URI - '/' because it's the root of the SQS service
+    var canonicalUri = uri || '/';
+
+    // Canonical Query String - encode the parameters in the URL
+    var canonicalQuerystring = Object.keys(request.params).sort().map(key => key + '=' + request.params[key]).join('&');
+
+    // Canonical Headers - include the 'host' header and the 'x-amz-date' header
+    var canonicalHeaders = 'host:' + host + '\n' + 'x-amz-date:' + amzDate + '\n';
+
+    // Signed Headers - 'host' and 'x-amz-date' in this case
+    var signedHeaders = 'host;x-amz-date';
+
+    var payload = request.body ? (typeof request.body === 'object' ? JSON.stringify(request.body) : request.body) : '';
+
+    // Payload hash - hash the empty string for GET requests
+    var payloadHash = CryptoJS.SHA256(payload).toString(CryptoJS.enc.Hex);
+
+    // Construct Canonical Request
+    var canonicalRequest = [
+      request.method, // HTTP request method (GET, POST, etc.)
+      canonicalUri,   // Canonical URI
+      canonicalQuerystring, // Canonical query string
+      canonicalHeaders,     // Canonical headers
+      signedHeaders,        // Signed headers
+      payloadHash           // Payload hash
+    ].join('\n');
+
+    // Hash the canonical request
+    var hashedCanonicalRequest = CryptoJS.SHA256(canonicalRequest).toString(CryptoJS.enc.Hex);
+
+    // Construct String-to-Sign
+    var stringToSign = [
+      algorithm,         // Algorithm used (AWS4-HMAC-SHA256)
+      amzDate,           // Date and time of the request (yyyyMMdd'T'HHmmss'Z')
+      credentialScope,   // Credential Scope (date/region/service/aws4_request)
+      hashedCanonicalRequest // Hashed Canonical Request
+    ].join('\n');
+
+    // Generate signing key
+    var kDate = CryptoJS.HmacSHA256(dateStamp, 'AWS4' + secretKey);
+    var kRegion = CryptoJS.HmacSHA256(region, kDate);
+    var kService = CryptoJS.HmacSHA256(service, kRegion);
+    var kSigning = CryptoJS.HmacSHA256('aws4_request', kService);
+
+    // Calculate the signature
+    var signature = CryptoJS.HmacSHA256(stringToSign, kSigning).toString(CryptoJS.enc.Hex);
+
+    // Authorization header
+    var authorizationHeader = algorithm + ' ' +
+      'Credential=' + accessKey + '/' + credentialScope + ', ' +
+      'SignedHeaders=' + signedHeaders + ', ' +
+      'Signature=' + signature;
+
+    // Return the signed headers
+    return {
+      'authorization': authorizationHeader,
+      'xAmzDate': amzDate,
+      'host': host
+    };
+  }
+
+  return { createSignature };
+}));
